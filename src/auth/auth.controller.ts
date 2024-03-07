e6157a622e5831a2a63f71f06df1f1fb9460072a
artur-cesar <arturcesarmelo@gmail.com>
2024-03-06 22:23:25 -0300

    Create file upload resource for auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { LoginDTO } from './dto/login.dto';
 import { RegisterDTO } from './dto/register.dto';
 import { ForgetPasswordDTO } from './dto/forget-password.dto';
@@ -6,10 +17,17 @@ import { ResetPasswordDTO } from './dto/reset-password.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UserDTO } from 'src/user/dto/user.dto';
+import { join } from 'path';
+import { FileSystemService } from 'src/file-system/file-system.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly fileSystemService: FileSystemService,
+  ) {}
 
   @Post('login')
   async login(@Body() { email, password }: LoginDTO) {
@@ -34,8 +52,43 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @Post('me')
   async me(@User() user) {
-    // return headers;
-    // return await this.authService.checkToken(headers.authorization.split(' ')[1]);
     return { user };
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(AuthGuard)
+  @Post('photo')
+  async photo(
+    @User() user: UserDTO,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: 'image/png' }),
+          new MaxFileSizeValidator({ maxSize: 1024 * Number(process.env.MAX_UPLOAD_FILE_SIZE_FACTOR) }),
+        ],
+      }),
+    )
+    photo: Express.Multer.File,
+  ) {
+    const dashUserName = user.name
+      .split(' ')
+      .map((i) => i.toLocaleLowerCase())
+      .join('-');
+
+    const filePath = join(
+      __dirname,
+      '..',
+      '..',
+      'storage',
+      'photos',
+      `photo-${dashUserName}-${user.id}.png`,
+    );
+
+    try {
+      await this.fileSystemService.upload(photo, filePath);
+      return { success: true, filePath };
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 }
